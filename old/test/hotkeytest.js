bf2e47d9b48d372539b755b3c32642c5cb7c944e
liu zhengdong <37199800+liuzhengdong2@users.noreply.github.com>
2021-06-26 03:16:08 +0800

    delete

--- old/test/hotkeytest.js
+++
@@ -1,7 +0,0 @@
-var hotkeys = require('hotkeys-js')
-
-hotkeys('f5', function(event, handler){
-  // Prevent the default refresh event under WINDOWS system
-  event.preventDefault() 
-  alert('you pressed F5!') 
-});
