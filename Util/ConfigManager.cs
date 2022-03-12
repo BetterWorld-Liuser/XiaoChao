07c2dce01b38c9d6c409ff6f1b590d9705db60b9
liu zhengdong <735083049@qq.com>
2022-03-12 16:01:33 +0800

    修改了配置文件导入的逻辑，采用字典的方式存储配置。

--- Util/ConfigManager.cs
+++ Util/ConfigManager.cs
@@ -9,6 +9,7 @@ using System.Reflection;
 
 namespace xiaochao
 {
+    //这个类采用单例模式
     public class ConfigManager
     {
         static private ConfigManager _instance;
@@ -34,71 +35,99 @@ namespace xiaochao
             get => Font_size - 7 ;
         }
 
+        //开始：ConfigManager初始化函数
         private ConfigManager()
         {
-            string config_file_path = Path.Combine(AppDomain.CurrentDomain.SetupInformation.ApplicationBase, "设置.md");
-            //找到配置文件的位置
-            if (File.Exists(config_file_path))
-            {
-                //找到
-                foreach (string line in File.ReadLines(config_file_path))
-                {
-                    string color_pattern = "^#[a-fA-F0-9]{6}$";
-                    string opacity_pattern = "[0-9]{1,3}";
-                    string line_a = line.Trim();
-                    string[] key_value = line_a.Split(' ');
-                    if (key_value.Length >= 2)
-                    {
-                        switch (key_value[0])
-                        {
-                            case "开机启动":switch (key_value[1])
-                                {
-                                    case "是": Start_Up = true; break;
-                                    case "否": Start_Up = false; break;
-                                }break;
-                            case "快捷键": Shortcut = key_value[1];break;
-                            case "背景色":if (Regex.IsMatch(key_value[1], color_pattern))
-                                {
-                                    Background_color = key_value[1];
-                                }break;
-                            case "透明度":if (Regex.IsMatch(key_value[1], opacity_pattern))
-                                {
-                                    double temp = double.Parse(key_value[1]);
-                                    if (temp > 0 & temp < 100)
-                                    {
-                                        Background_opacity = temp / 100;
-                                    }
-                                }break;
-                            case "字体颜色":if (Regex.IsMatch(key_value[1], color_pattern))
-                                {
-                                    Font_color = key_value[1];
-                                }break;
-                            case "装饰色":if (Regex.IsMatch(key_value[1], color_pattern))
-                                {
-                                    Decoration_color = key_value[1];
-                                }break;
-                            case "基准大小": if(Regex.IsMatch(key_value[1], opacity_pattern))
-                                {
-                                    Font_size = int.Parse(key_value[1]);
-                                }break;
-                        }
+            
+            Dictionary<string, string> config_dictionary = new Dictionary<string, string>();
+            config_dictionary = LoadConfig();
+            InitSetting(config_dictionary);
 
-                                
-                        
-                        
-                        
-                        
-                        
-                    }
-                    
-                }
-
-            }
         }
+        //结束：ConfigManager初始化函数
 
         static public ConfigManager GetInstance()
         {
             return _instance ?? (_instance = new ConfigManager());
         }
+
+
+        /// <summary>
+        /// 导入配置文件
+        /// </summary>
+        /// <returns></returns>
+        public Dictionary<string, string> LoadConfig()
+        {
+            //开始：找到配置文件并导入
+            Dictionary<string, string> config_dictionary = new Dictionary<string, string>();
+            string config_file_path = Path.Combine(AppDomain.CurrentDomain.SetupInformation.ApplicationBase, "设置.md");
+            if (File.Exists(config_file_path))
+            {
+                string color_pattern = "^#[a-fA-F0-9]{6}$";
+                string opacity_pattern = "[0-9]{1,3}";
+                //开始：循环遍历每一行元素，将其加入字典中
+                foreach (string line in File.ReadLines(config_file_path))
+                {
+
+                    string lineTrimed = line.Trim();
+                    string[] key_value = lineTrimed.Split(' ');
+                    //将每个键值都加入字典中
+                    if (key_value.Length >= 2) config_dictionary.Add(key_value[0], key_value[1]);
+
+
+                }//结束：循环遍历每一行元素，将其加入字典中
+            }
+            //结束：找到配置文件并导入
+            return config_dictionary;
+         }
+
+
+        /// <summary>
+        /// 初始化设置的各种值
+        /// </summary>
+        /// <param name="config_dictionary"></param>
+        public void InitSetting(Dictionary<string, string> config_dictionary)
+        {
+            //设置开机启动
+            string start_up;
+            if (config_dictionary.TryGetValue("开机启动", out start_up))
+            {
+                if (start_up == "否") Start_Up = false;
+            }
+
+            //设置快捷键
+            string shortcut;
+            if (config_dictionary.TryGetValue("快捷键", out shortcut)) Shortcut = shortcut;
+
+
+            //设置背景色
+            string backgroundcolor;
+            if (config_dictionary.TryGetValue("背景色", out backgroundcolor)) Background_color = backgroundcolor;
+
+            //设置透明度
+            string opacity;
+            if (config_dictionary.TryGetValue("透明度", out opacity))
+            {
+                double temp = double.Parse(opacity);
+                if (temp > 0 & temp < 100) Background_opacity = temp / 100;
+            }
+
+            //设置字体颜色
+            string font_color;
+            if (config_dictionary.TryGetValue("字体颜色", out font_color)) Font_color = font_color;
+
+            //设置装饰色
+            string decorationcolor;
+            if (config_dictionary.TryGetValue("装饰色", out decorationcolor)) Decoration_color = decorationcolor;
+
+            //设置基准大小
+            string basesize;
+            if (config_dictionary.TryGetValue("基准大小", out basesize))
+            {
+                Font_size = int.Parse(basesize);
+            }
+
+        }
+
     }
 }
